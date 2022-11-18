d602f94421d86acb5c59a0a3a78b4dd013c77c2d
wirert <wirert@students.softuni.bg>
2022-11-18 13:58:58 +0200

    2. ValidationAttributes

---
+++ 07.1.ReflectionAndAttributes/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,32 @@
+﻿namespace ValidationAttributes.Utilities
+{
+    using Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object classInstance)
+        {            
+            var props = classInstance.GetType().GetProperties();
+
+            foreach (var prop in props)
+            {
+                var attrs = prop.GetCustomAttributes(typeof(MyValidationAttribute), true);
+                var propValue = prop.GetValue(classInstance);
+
+                foreach (var attr in attrs)
+                {
+                    var attrMethod = attr.GetType().GetMethod("IsValid");
+
+                    bool isValid = (bool)attrMethod.Invoke(attr, new object[] { propValue });
+
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+                }
+            }     
+
+            return true;
+        }
+    }
+}
