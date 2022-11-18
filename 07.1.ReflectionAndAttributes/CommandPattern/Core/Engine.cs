832786ca24109233a9492b06f8b6162047a7da46
wirert <wirert@students.softuni.bg>
2022-11-18 12:06:01 +0200

    1. 1. Command Pattern

---
+++ 07.1.ReflectionAndAttributes/CommandPattern/Core/Engine.cs
@@ -0,0 +1,44 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+
+    using Contracts;
+    using IO;
+    using IO.Contracts;
+
+    public class Engine : IEngine
+    {
+        private readonly IReader reader;
+        private readonly IWriter writer;
+        private readonly ICommandInterpreter commandInterpreter;
+
+        private Engine()
+        {
+            reader = new ConsoleReader();
+            writer = new ConsoleWriter();
+        }
+
+        public Engine(ICommandInterpreter commandInterpreter)
+            : this()
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            string input;
+
+            while ((input = reader.ReadLine()) != "End")
+            {
+                try
+                {
+                    writer.WriteLine(commandInterpreter.Read(input));
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    writer.WriteLine(ioe.Message);
+                }                
+            }            
+        }
+    }
+}
---
+++ 07.1.ReflectionAndAttributes/CommandPattern/Utilities/CommandInterpreter.cs
@@ -0,0 +1,35 @@
+﻿namespace CommandPattern.Utilities
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Core.Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string cmdArgs)
+        {
+            string commandName = cmdArgs.Split()[0];
+            string[] args = cmdArgs.Split().Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            if (type == null)
+            {
+                throw new InvalidOperationException("Invalid Command type!");
+            }
+
+            var instance = Activator.CreateInstance(type);
+            MethodInfo method = type.GetMethod("Execute");
+
+            if (method == null)
+            {
+                throw new InvalidOperationException($"ICommand pattern isn't implemented in {commandName}Command class.");
+            }
+
+            return method.Invoke(instance, new object[] { args }) as string;
+        }
+    }
+}
