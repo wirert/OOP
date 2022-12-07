fadc7336f4b6d77691a7fb0d49270be945da19e8
wirert <wirert@students.softuni.bg>
2022-12-07 17:27:02 +0200

    task 3 unit test

--- Exam Exercises/13. OOP Retake Exam 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
+++ Exam Exercises/13. OOP Retake Exam 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
@@ -1,20 +1,75 @@
 using NUnit.Framework;
+using System;
 using TheRace;
 
 namespace TheRace.Tests
 {
     public class RaceEntryTests
     {
+        private string driverName;
+        private UnitDriver driver;
+        private RaceEntry raceEntry;        
 
         [SetUp]
         public void Setup()
         {
+            driverName = "Pesho";
+            raceEntry = new RaceEntry();            
+            driver = new UnitDriver(driverName, new UnitCar("Model", 200, 2000));
         }
 
         [Test]
-        public void TestOne()
+        public void CtorAndCounterTest()
         {
-            Assert.Pass();
+            Assert.IsTrue(raceEntry.Counter == 0);
+        }
+
+        [Test]
+        public void AddDriverShouldThrowIfDriverIsNull()
+        {
+            UnitDriver nullDriver = null;
+
+            Assert.Throws<InvalidOperationException>(() => raceEntry.AddDriver(nullDriver));
+        }
+
+        [Test]
+        public void AddDriverShouldThrowIfExistDriverName()
+        {
+            raceEntry.AddDriver(driver);
+
+            UnitDriver sameNameDriver = new UnitDriver(driverName, new UnitCar("new", 500, 4000));
+
+            Assert.Throws<InvalidOperationException>(() => raceEntry.AddDriver(sameNameDriver));
+        }
+
+        [Test]
+        public void AddDriver_Test()
+        {
+            string expectedOutput = $"Driver {driverName} added in race.";
+
+            Assert.AreEqual(expectedOutput, raceEntry.AddDriver(driver));
+            Assert.IsTrue(raceEntry.Counter == 1);
+        }
+
+        [Test]
+        public void CalculateAverageHorsePowerThrowsIfParticipantsLessThanTwo() 
+        {
+            raceEntry.AddDriver(driver);
+
+            Assert.Throws<InvalidOperationException>(() => raceEntry.CalculateAverageHorsePower());
+        }
+
+        [Test]
+        public void CalculateAverageHorsePower_Test()
+        {
+            raceEntry.AddDriver(driver);
+
+            var driverTwo = new UnitDriver("Gosho", new UnitCar("newModel", 300, 2500));
+            raceEntry.AddDriver(driverTwo);
+
+            double expectedAveragePower = (200 + 300) / 2;
+
+            Assert.AreEqual(expectedAveragePower, raceEntry.CalculateAverageHorsePower());
         }
     }
 }
